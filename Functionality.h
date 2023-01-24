0e8c184f696584198f8e4761a5d0320aae4fee1d
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-24 20:01:39 +0530

    Update Functionality.h

--- Functionality.h
+++ Functionality.h
@@ -2,4 +2,4 @@
 #define CSVFormat   "Range, Readings"
 void doSort(int *A,int len );
 int CountValue(int *A,int len, int low,int high);
-void CreateInformat(char* buff);
+void CreateInformat(int lowLimit,int highLimit,int counter, char* buff)
