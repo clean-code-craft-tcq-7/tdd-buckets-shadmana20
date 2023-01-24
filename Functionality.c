a1508d851730b7db6aa2523bca489b6a3cf5e352
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-24 20:14:44 +0530

    Update Functionality.c

--- Functionality.c
+++ Functionality.c
@@ -2,8 +2,11 @@
 #include <string.h>
 #include "Functionality.h"
 
+
 void CreateInformat(int lowLimit,int highLimit,int counter, char* buff)
 {
+  
+char Data[100];
 sprintf(Data,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
 strncpy(buff,Data,strlen(Data));
 }
