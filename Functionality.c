e876f1e3fd8fa3f769303561b2f7d8a34714e569
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-24 19:49:23 +0530

    Update Functionality.c

--- Functionality.c
+++ Functionality.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include "Functionality.h"
+
+void createFormat(void)
+{
+sprintf(Data,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
+strncpy(buff,Data,strlen(Data));
+}
