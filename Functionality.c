1cb98f819ca3c7fb4c20372bcc92fb57098a6b8e
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-26 18:37:38 +0530

    Update Functionality.c
    updated the core functionality.

--- Functionality.c
+++ Functionality.c
@@ -9,3 +9,33 @@ char Data[100];
 sprintf(Data,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
 strncpy(buff,Data,strlen(Data));
 }
+
+ void doSort(int *A,int len )
+{
+   int tmp;
+    for(int i=0;i<len;i++)
+    {
+        for(int j=0;j<(len-i-1);j++)
+        {
+            if(A[j]>A[j+1])
+            {
+                tmp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = tmp;
+            }
+        }
+    }
+}
+
+int CountValue(int *A,int len, int low,int high)
+{
+    int cnt = 1;
+    for(int i=0;i<=len;i++)
+    {
+        if(A[i]>=low && A[i]<=high)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+} 
