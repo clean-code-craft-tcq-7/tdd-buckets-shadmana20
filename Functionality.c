035b83b51f11b2249ad9135fa8e22dac1e8047ab
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-26 18:52:07 +0530

    Update Functionality.c

--- Functionality.c
+++ Functionality.c
@@ -11,31 +11,29 @@ strncpy(buff,Data,strlen(Data));
 }
  
 void doSort(int *A,int len)
-{
-   int tmp,i,j;
-    for(i=0;i<len;i++)
-    {
-        for(j=0;j<(len-i-1);j++)
-        {
-            if(A[j]>A[j+1])
-            {
-                tmp = A[j];
-                A[j] = A[j+1];
-                A[j+1] = tmp;
-            }
-        }
-    }
+{int tmp,i,j;
+ for(i=0;i<len;i++)
+ {
+   for(j=0;j<(len-i-1);j++)
+   {
+     if(A[j]>A[j+1])
+     {
+       tmp = A[j];
+       A[j] = A[j+1];
+       A[j+1] = tmp;
+     }
+   }
+ }
 }
-
 int CountValue(int *A,int len, int low,int high)
 {
-    int i,cnt = 1;
-    for(i=0;i<=len;i++)
-    {
-        if(A[i]>=low && A[i]<=high)
-        {
-            cnt++;
-        }
-    }
-    return cnt;
+  int i,cnt = 1;
+  for(i=0;i<=len;i++)
+  {
+    if(A[i]>=low && A[i]<=high)
+    {
+      cnt++;
+    }
+  }
+  return cnt;
 }
