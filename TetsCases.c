3de12cdc463ce81feb835118f0cbcb4a10bffbe6
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-30 20:13:27 +0530

    Update TetsCases.c

--- TetsCases.c
+++ TetsCases.c
@@ -2,17 +2,28 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include "Functionality.h"
+#include "Implementation.h"
 
 
 int main(void)
 {
+ 
+ int current_samples[7]={3, 3, 5, 4, 10, 11, 12};
+ int storecount;
  char output[100];
  memset(output, 0, 100);
  
-CreateInformat(4,5,2, output);
+
+doSort(current_samples,7);
+storecount = CountValue(current_samples,7,3,5);
+CreateInformat(3,5,storecount,output);
  
- assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+ printf("%s\n", output);
+ 
+ storecount = CountValue(current_samples,7,10,12);
+ CreateInformat(10,12,storecount,output);
+  printf("%s", output);
+// assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
   
 return 0;
   
