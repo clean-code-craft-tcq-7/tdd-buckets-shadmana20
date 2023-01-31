9bdf57ce308033cbd7fd337089dbd0dc08731880
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-31 10:20:25 +0530

    Update TetsCases.c

--- TetsCases.c
+++ TetsCases.c
@@ -17,13 +17,13 @@ int main(void)
 doSort(current_samples,7);
 storecount = CountValue(current_samples,7,3,5);
 CreateInformat(3,5,storecount,output);
+printf("%s\n", output);
+assert(strcmp(output,"Range, Readings\n3-5, 4\n") == 0);
  
- printf("%s\n", output);
- 
- storecount = CountValue(current_samples,7,10,12);
- CreateInformat(10,12,storecount,output);
-  printf("%s", output);
-// assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+storecount = CountValue(current_samples,7,10,12);
+CreateInformat(10,12,storecount,output);
+printf("%s", output);
+assert(strcmp(output,"Range, Readings\n10-12, 3\n") == 0);
   
 return 0;
   
