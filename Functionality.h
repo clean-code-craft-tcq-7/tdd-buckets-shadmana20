8bce9adff611ffcfd5d7a0703923b05ab8f64023
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-01-31 19:40:14 +0530

    Update Functionality.h

--- Functionality.h
+++ Functionality.h
@@ -3,5 +3,6 @@
 void doSort(int *A,int len );
 int CountValue(int *A,int len, int low,int high);
 void CreateInformat(int lowLimit,int highLimit,int counter, char* buff);
+void PerformTest(int *A,int len,int min,int max,char* buff);
 
 
