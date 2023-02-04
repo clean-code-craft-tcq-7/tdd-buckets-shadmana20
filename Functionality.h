c94ef50fd196c0e77c2803b9acda98b04681ae19
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-02-04 12:15:49 +0530

    Update Functionality.h

--- Functionality.h
+++ Functionality.h
@@ -5,7 +5,6 @@ int CountValue(int *A,int len, int low,int high);
 void CreateInformat(int lowLimit,int highLimit,int counter, char* buff);
 void PerformTest(int *A,int len,int min,int max,char* buff);
 #define LEN(A) sizeof(A)/sizeof(A[0])
-#define Converter(x)  ((int)(x*1000)%1000)
 int A2DConverter(int LSBValue);
 
 
